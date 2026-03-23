87c3cc5908e1c3bc42cf7c7011f61a0b3ebd68a4
Christian Zimpel de Lima <christian.zimpel@gmail.com>
2026-03-23 11:37:38 -0300

    Reducer aplicado ao código

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -8,9 +8,10 @@ import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
 import { getNextCycle } from "../../utils/getNextCycle";
 import { getNextCycleType } from "../../utils/getNextCycleType";
 import { formatSecondsToMinutes } from "../../utils/formatSecondsToMinutes";
+import { TaskActionTypes } from "../../contexts/TaskContext/TaskActions";
 
 export function MainForm() {
-  const { state, setState } = useTaskContext();
+  const { state, dispatch } = useTaskContext();
   const taskNameInput = useRef<HTMLInputElement>(null);
 
   // ciclos
@@ -39,38 +40,14 @@ export function MainForm() {
       type: nextCycleType,
     };
 
-    const secondsRemaining = newTask.duration * 60; // Para transformar de minutos em segundos
-
-    setState((prevState) => {
-      return {
-        ...prevState,
-        activeTask: newTask,
-        currentCycle: nextCycle,
-        secondsRemaining, // Conferir mais tarde
-        formattedSecondsRemaining: formatSecondsToMinutes(secondsRemaining), // Conferir mais tarde
-        tasks: [...prevState.tasks, newTask],
-      };
-    });
+    dispatch({type: TaskActionTypes.START_TASK, payload: newTask})
   }
 
   function handleInterruptTask(
     e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
   ) {
     e.preventDefault(); // Corrige o bug do react de reutilizar o submit
-    setState((prevState) => {
-      return {
-        ...prevState,
-        activeTask: null,
-        secondsRemaining: 0,
-        formattedSecondsRemaining: "00:00",
-        tasks: prevState.tasks.map((task) => {
-          if (prevState.activeTask && prevState.activeTask.id === task.id) {
-            return { ...task, interruptDate: Date.now() };
-          }
-          return task;
-        }),
-      };
-    });
+    dispatch({type: TaskActionTypes.INTERRUPT_TASK})
   }
 
   return (
--- src/contexts/TaskContext/TaskContextProvider.tsx
+++ src/contexts/TaskContext/TaskContextProvider.tsx
@@ -1,34 +1,23 @@
 import { useEffect, useReducer, useState } from "react";
 import { initialTaskState } from "./initialTaskState";
 import { TaskContext } from "./TaskContext";
+import { taskReducer } from "./taskReducer";
 
 type TaskContextProviderProps = {
   children: React.ReactNode;
 };
 
 export function TaskContextProvider({ children }: TaskContextProviderProps) {
-  const [state, setState] = useState(initialTaskState);
-
-  const [numero, dispatch] = useReducer((state, action) => {
-    console.log(state, action)
-
-    switch(action){
-      case 'INCREMENT':
-        return state + 1
-    }
-    return state;
-  }, 0)
+  const [state, dispatch] = useReducer(taskReducer, initialTaskState);
 
   // Para debugar as tasks
-  /* useEffect(() => {
+  useEffect(() => {
     console.log(state);
-  }, [state]); */
+  }, [state]);
 
   return (
-    <TaskContext.Provider value={{ state, setState }}>
-      {/* {children} */}
-      <h1>o numero é {numero}</h1>
-      <button onClick={() => dispatch('INCREMENT')}>Incrementar</button>
+    <TaskContext.Provider value={{ state, dispatch }}>
+      {children}
     </TaskContext.Provider>
   );
 }
